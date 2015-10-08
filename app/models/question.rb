e74f357d42c85f1da2db429c0e4c37836125b556
Austin <admin@designxdevelop.com>
2015-10-08 10:17:20 -0700

    Assignment 31 attempted w/ errors

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,3 @@
 class Question < ActiveRecord::Base
-  has_many: answers # => why is this answers when the actual model is just Answer?
+  has_many :answers # => why is this answers when the actual model is just Answer?
 end
