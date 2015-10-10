44b79874fbdea72b0426da9cf71ab16d54403007
Austin <admin@designxdevelop.com>
2015-10-10 13:40:36 -0700

    CP 34 assignment attempt

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,12 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    # => Assignment 34 Attempt // Not really sure how to pull this off
+    index = 0
+    @posts.each_with_index do |title,index|
+      index += 5
+      @posts.title = "SPAM" # => best guess but this would replace every title correct?
+    end
   end
 
   def show
