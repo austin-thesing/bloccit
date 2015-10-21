e1b7bc01a0116b8cf22b657018b60fe62daeedbf
Austin <admin@designxdevelop.com>
2015-10-21 13:53:09 -0700

    CP 32 || Complete with error on posts show view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,9 +4,25 @@ class PostsController < ApplicationController
   end
 
   def show
+    @posts = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new # creates a new instance of Post
+    @post.title = params[:post][:title] # what are the params and why are we using them
+    @post.body = params[:post][:body]
+
+    if @post.save
+      flash[:notice] = "Post was saved"
+      redirect_to @post
+    else
+      flash[:error] = "There was an error saving the post. Please try again."
+      render :new # does this just render a new form?
+    end
   end
 
   def edit
