bc6ac6c6280fe851bb9609198ef26090b0ac3dfb
Austin <admin@designxdevelop.com>
2015-10-22 08:21:04 -0700

    Assignment 32 Completed

--- app/controllers/advertisements_controller.rb
+++ app/controllers/advertisements_controller.rb
@@ -1,14 +1,29 @@
 class AdvertisementsController < ApplicationController
-  def index
+  def index # shows all the advertisments
     @advertisements = Advertisement.all
   end
 
   def show
   end
 
-  def new
+  def new # creates a new ad but does not save it to the database yet
+    @advertisement = Advertisement.new
   end
 
-  def create
+  def create # creates and pushes through a new ad and saves it into the database
+    @advertisement = Advertisement.new
+
+    @advertisement.title = params[:advertisement][:title]
+    @advertisement.copy = params[:advertisement][:copy]
+    @advertisement.price = params[:advertisement][:price]
+
+    if @advertisement.save
+      flash[:notice] = "Advertisement was created and saved successfully."
+      redirect_to @advertisement
+    else
+      flash[:error] = "Unable to save advertisement please try again later"
+      render :new
+    end
+
   end
 end
--- spec/controllers/advertisements_controller_spec.rb
+++ spec/controllers/advertisements_controller_spec.rb
@@ -1,4 +1,6 @@
 require 'rails_helper'
+include RandomData
+
 
 RSpec.describe AdvertisementsController, type: :controller do
 
@@ -18,8 +20,16 @@ let (:my_advertisement) { Advertisement.create!(title: RandomData.random_sentenc
 
   describe "GET #show" do
     it "returns http success" do
-      get :show
+      get :show, {id: my_advertisement.id}
       expect(response).to have_http_status(:success)
+    end
+    it "renders #show" do
+      get :show, {id: my_advertisement.id}
+      expect(response).to render_template :show
+    end
+    it "assigns my_advertisement to @advertisement" do
+      get :show, {id: my_advertisement.id}
+      expect(assigns(:advertisement)).to eq(my_advertisement)
     end
   end
 
@@ -40,10 +50,21 @@ let (:my_advertisement) { Advertisement.create!(title: RandomData.random_sentenc
     end
   end
 
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+  describe "POST #create" do # => pay attention to the CRUD/HTTP Actions in these make sure post is used for creating data
+    # it "returns http success" do
+    #   get :create
+    #   expect(response).to have_http_status(:success)
+    # end
+    it "increases the number of Advertisments by one" do
+      expect{post :create, advertisement: {title: RandomData.random_sentence, copy: RandomData.random_sentence, price: RandomData.random_int}}.to change(Advertisement,:count).by(1)
+    end
+    it "assigns the new advert to @advertisement" do
+      post :create, advertisement: {title: RandomData.random_sentence, copy: RandomData.random_paragraph, price: RandomData.random_int}
+      expect(assigns(:advertisement)).to eq Advertisement.last
+    end
+    it "redirects to the new advertisement" do
+      post :create, advertisement: {title: RandomData.random_sentence, copy: RandomData.random_paragraph, price: RandomData.random_int}
+      expect(response).to redirect_to Advertisement.last
     end
   end
 
