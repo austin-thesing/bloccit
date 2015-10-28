13ae860c0cfd58f426565beb6126fe0dd8cdc8ca
Austin <admin@designxdevelop.com>
2015-10-28 16:26:44 -0700

    added topic validation

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,8 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy # deletes posts that fall under a topic when the topic is deleted
+
+  # assignment 35 attempt
+  validates :name, length: {minimum: 5}, presence: true
+  validates :description, length: {minimum: 15}, presence: true
+  # REMEMBER!!! --- you need to make sure that your validations are validating the proper things or you will have spec failures
 end
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -5,6 +5,15 @@ RSpec.describe Topic, type: :model do
   let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)}
   it { should have_many(:posts) } # needs gem 'shoulda' for it to work
 
+# checks to make sure title,description are present
+it {should validate_presence_of(:name)}
+it {should validate_presence_of(:description)}
+
+
+# checks the length of title and body
+it {should validate_length_of(:name).is_at_least(5)}
+it {should validate_length_of(:description).is_at_least(15)}
+
   describe "attributes" do
     it "responds to name" do
       expect(topic).to respond_to(:name)
