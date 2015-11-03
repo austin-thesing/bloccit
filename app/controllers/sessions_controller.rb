4f9257c7ffcb0dbefa209b8c0380570350d511a8
Austin <admin@designxdevelop.com>
2015-11-02 18:36:58 -0800

    CP 38 partial
    Create & Destroy methods coded in SessionsController

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  def New
+
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase) # => searches the DB for a user email in the params hash. Email is downcased so that it matches format of database.
+
+    if user && user.authenticate(params[:session][:password])
+      create_session user
+      flash[:notice] = "Welcome #{user.name}"
+      redirect_to root_path # => takes us back to the home page or welcome#index view
+    else
+      flash[:error] = "Invalid username/password. Please try again."
+      render :new
+    end
+  end
+
+  def destroy
+    flash[:notice] = "You've been signed out, come back soon!"
+    redirect_to root_path # => takes us back to the home page or welcome#index view
+  end
+
+end
