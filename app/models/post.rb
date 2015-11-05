da8f469757bafd7a8eb990baa4341b58eebfa365
Austin <admin@designxdevelop.com>
2015-11-05 15:43:13 -0800

    CP 39 assignment complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,9 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy # => the dependent: :destroy means that when a post is deleted it will delete the comments on the post
 
   default_scope { order('created_at DESC') }
+  
+  scope :ordered_by_title, -> { order('title DESC')}
+  scope :ordered_by_reverse_created_at, -> { order('created_at ASC')} # might have to use #reload! after you change a model
 
   validates :title, length: { minimum: 5}, presence: true
   validates :body, length: { minimum: 20}, presence: true
