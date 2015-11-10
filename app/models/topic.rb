af58def65951b9024a483aa3af90560c6f20f8cf
Austin <admin@designxdevelop.com>
2015-11-09 18:15:46 -0800

    CP 42 Complete added Labels

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,6 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy # deletes posts that fall under a topic when the topic is deleted
+
+  has_many :labelings, as: :labelable
+  has_many :labels, through: :labelings 
 end
