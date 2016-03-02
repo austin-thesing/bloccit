c0609ebf7b14c18fd455fa798b9e7985ba537081
Austin <admin@designxdevelop.com>
2016-03-02 14:31:53 -0800

    testing travis ci

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
+RSpec.describe User, type: :model do 
   let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password")}
   it {should have_many(:posts)}
   it {should have_many(:comments)}
