2ac667f82dd2c8240739f7f37633ec8819a391e9
Austin <admin@designxdevelop.com>
2016-03-12 15:10:42 -0800

    CP 46 completed, Private Topics are now hidden from guest users

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,6 +1,7 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy # deletes posts that fall under a topic when the topic is deleted
-
   has_many :labelings, as: :labelable
-  has_many :labels, through: :labelings 
+  has_many :labels, through: :labelings
+
+  scope :visible_to, -> (user) { user ? all : where(public: true) } # '->' passes code into the scope
 end
