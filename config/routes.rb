8c6a819d1c241cb00b4e9783f1a2616afb7dccc1
Austin <admin@designxdevelop.com>
2016-03-12 17:22:55 -0800

    CP 48 completed

--- config/routes.rb
+++ config/routes.rb
@@ -21,4 +21,10 @@ Rails.application.routes.draw do
 
   root to: 'welcome#index' # => sets the root or the file that loads when you go to your site/app
 
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :show]
+      resources :topics, only: [:index, :show]
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -68,7 +68,7 @@ RSpec.describe User, type: :model do
         expect(user.member?).to be_falsey
       end
       it "should return true for admin?" do
-        expect(user.member?).to be_truthy
+        expect(user.admin?).to be_truthy
       end
     end
   end
@@ -126,6 +126,11 @@ RSpec.describe User, type: :model do
     it "returns the proper gravatar URL for a known email entity" do
       expected_gravatar = "http://gravatar.com/avatar/bb6d1172212c180cfbdb7039129d7b03.png?s=48"
       expect(known_user.avatar_url(48)).to eq(expected_gravatar)
+    end
+  end
+  describe "#generate_auth_token" do
+    it "creates a token" do
+      expect(user.auth_token).to_not be_nil
     end
   end
 end
